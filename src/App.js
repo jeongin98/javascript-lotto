fcfe3e4b44dbf52c46e0e4023ff6e45f516d7e11
jeongin98 <eon7500@gmail.com>
2023-09-04 18:53:13 +0900

    feat:[1] 3개 숫자 랜덤 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils"); // 우테코 API
+
+
 class App {
-  play() { }
+
+  setRandomNum() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const randomNum = this.setRandomNum();
+  }
+
 }
+
+
+const app = new App();
+app.play();
 
 module.exports = App;
