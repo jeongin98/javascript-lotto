8313f61cb84282c55d4815c4e4192d7514f313ed
jeongin98 <eon7500@gmail.com>
2023-09-11 07:24:31 +0900

    feat: 유저의 입력 유효성 검사

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ class App {
     this.userMoney;
   }
 
+  checkUserInput(userInput) {
+    if (userInput % 1000 != 0)
+      throw new Error("입력하신 금액은 1000 단위가 아닙니다.");
+
+    return;
+  }
+
   getUserInput() {
     MissionUtils.Console.readLine('구입금액을 입력해주세요.', (userInput) => {
       this.checkUserInput(userInput);
       this.userMoney = userInput;
     });
   }
+
 
 
   play() {
