da8302e6caa26165d99dfe02e1760532e4dbe320
jeongin98 <eon7500@gmail.com>
2023-09-11 07:21:19 +0900

    feat: 유저로부터  금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 class App {
-  play() {}
+
+  constructor() {
+    this.userMoney;
+  }
+
+  getUserInput() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요.', (userInput) => {
+      this.checkUserInput(userInput);
+      this.userMoney = userInput;
+    });
+  }
+
+
+  play() {
+    this.getUserInput();
+  }
 }
 
 module.exports = App;
