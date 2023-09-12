fd9820aa9d19bfdcad37e622767e6560bb7bcea7
jeongin98 <eon7500@gmail.com>
2023-09-12 14:06:36 +0900

    feat: 맞춘 로또 번호 개수 카운트

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ class App {
 
   constructor() {
     this.userMoney;
-    this.wholeLottoNum = []; // 이중 배열인 상태로 저장됨
+    this.wholeLottoList = []; // 이중 배열인 상태로 저장됨
     this.userLottoNum = [];
     this.bonusNumber;
-
+    this.matchSix = 0;
+    this.matchFiveAndBonus = 0;
+    this.matchFive = 0;
+    this.matchFour = 0;
+    this.matchThree = 0;
+    this.rateOfReturn = 0; // rateOfReturn을 클래스 멤버 변수로 선언
 
   }
   getUserInput() {
@@ -24,23 +29,19 @@ class App {
     console.log("playTimes : " + playTimes);
     // n번 반복
     for (let i = 0; i < playTimes; i++) {
-      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6); // 배열 반환
       // 오름차순
-      this.wholeLottoNum.push(numbers.sort(function (a, b) {
+      this.wholeLottoList.push(numbers.sort(function (a, b) {
         return a - b;
       }));
-      console.log("전체 : " + this.wholeLottoNum);
+      console.log("전체 : " + this.wholeLottoList);
     }
     this.getUserLotto();
   }
 
-
-
-
-
   getUserLotto() {
     MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (userInput) => {
-      this.userLottoNum = userInput.split(',');
+      this.userLottoNum = userInput.split(','); // 배열 반환
       // Lotto 클래스에서 유효성 검사 
       new Lotto(this.userLottoNum);
       this.getUserBonusNum(); // 여기서 호출이 맞나
@@ -62,33 +63,52 @@ class App {
   }
 
 
-  calculateReward() {
-    // let matchCount = 0; // 일치하는 숫자 카운트 변수 초기화
+  // 정산 및 수익률 계산 함수
+  calculateMoneyReward(matchCount, bonusSame) {
+
+    // let matchSix = 0, matchFiveAndBonus = 0, matchFive=0,matchFour = 0, matchThree = 0; // 전역 멤버로
+
+    if (matchCount == 6)
+      this.matchSix++;
+    if (matchCount == 5 && bonusSame == true)
+      this.matchFiveAndBonus++;
+    if (matchCount == 5)
+      this.matchFive++;
+    if (matchCount == 4)
+      this.matchFour++;
+    if (matchCount == 3)
+      this.matchThree++;
 
 
-    for (let oneLotto of this.wholeLottoNum) {
-      let matchCount = 0;
-      let bonusSame = false;
-      console.log("oneLotto  : " + oneLotto);
-      //for (let digit of oneLotto) {
-      if (oneLotto.filter(x => this.userLottoNum.includes(x)))
-        matchCount++; // 일치하는 숫자 발견 시 카운트 증가
+    const sum = (5000 * this.matchThree) + (50000 * this.matchFour) + (1500000 * this.matchFive) + (30000000 * this.matchFiveAndBonus) + (2000000000 * this.matchSix);
+    // 수익률 계산
+    this.rateOfReturn = (sum / this.userMoney) * 100;
 
-      console.log("matchCount : " + matchCount);
-      // 보너스 체크 -> true로 바꾸기
-      console.log("this.bonusNumber : " + this.bonusNumber);
-      if (oneLotto.includes(this.bonusNumber)) {
-        bonusSame = true;
-
-        console.log("bonusSame1 : " + bonusSame);
-      }
-
-      console.log("bonusSame2 : " + bonusSame);
-      //this.jeongsan(matchCount, bonusSame);
-      // }
-      console.log("일치 개수 : " + matchCount);
-    }
   }
+
+  // 오류 함수 코드
+  calculateReward() {
+    let bonusSame = false;
+
+    // 문제상황 
+    // 1) 맞춘 로또 번호 개수인 mathCount가 계속 0만 나옴. 
+    // 2) 보너스점수 일치 여부가 확인이 안 됨. 마찬가지로 includes 문이 반응 안 함
+
+    // this.wholeLottoNum와 oneLotto와 this.userLottoNum 와 oneLottoList 모두 배열 맞음(isArray 결과시 모두 true 나옴)
+    // oneLottoList에 저장된 것들도 Number로, 문제 없음
+    // 배열 내용도 잘 저장되어 있는 상황
+    this.wholeLottoList.forEach((oneLottoList) => {
+      let matchCount = oneLottoList.filter((nums) => this.userLottoNum.includes(nums)).length; // 맞춘 로또 숫자 개수 반환
+      console.log("matchCount : " + matchCount); // 계속 0만 나옴 ㅠㅠ
+      if (oneLottoList.includes(this.bonusNumber)) { // 실행이 안 됨
+        bonusSame = true;
+      }
+      this.calculateMoneyReward(matchCount, bonusSame);
+    });
+
+  }
+
+
 
   checkUserInput(userInput) {
     if (userInput % 1000 != 0)
@@ -96,9 +116,11 @@ class App {
     return;
   }
 
+
   play() {
     this.getUserInput();
   }
+
 }
 
 const app = new App();
