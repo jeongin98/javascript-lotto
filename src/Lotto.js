d2690ec161c9c5a30a6073ac3647259819fb9219
jeongin98 <eon7500@gmail.com>
2023-10-08 14:28:00 +0900

    feat(Lotto.js): 랜덤 로또 번호 유효성검사 구현

---
+++ src/Lotto.js
@@ -0,0 +1,31 @@
+class Lotto {
+    #numbers;
+
+    constructor(numbers) {
+        this.validate(numbers);
+        this.#numbers = numbers;
+    }
+
+    validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        const numPattern = /^[0-9]*$/;
+        if (numPattern.test(numbers)) {
+            throw new Error("[ERROR] 숫자만 입력하세요.");
+        }
+        const uniqueNums = new Set(numbers);
+        if (uniqueNums.size !== numbers.length) {
+            throw new Error("[ERROR] 중복 되지 않는 숫자를 입력하세요.");
+        }
+        numbers.forEach((number) => {
+            if (number < 1 || number > 45) {
+                throw new Error("[ERROR] 1~45 사이에 숫자만 입력하세요.");
+            }
+        });
+    }
+
+    // TODO: 추가 기능 구현 // ReCheckPlz
+}
+
+module.exports = Lotto;
